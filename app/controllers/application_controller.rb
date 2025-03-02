d147f46bd3bee6e6045626b98160e75a6030daed
YUSRA <111371895+yrasool@users.noreply.github.com>
2025-03-02 13:46:32 -0500

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
-class RoutesController < ApplicationController
-  def index
-    @routes = ExternalApiService.get_routes
-  end
-  
-  def show
-    @route = ExternalApiService.get_route(params[:id])
-    @stops = ExternalApiService.get_route_stops(params[:id])
-  end
+class ApplicationController < ActionController::Base
+  # Shared controller logic goes here
 end
