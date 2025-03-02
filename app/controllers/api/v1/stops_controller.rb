b98a01333e0edd198a1dd1cfca479c37ea648074
YUSRA <111371895+yrasool@users.noreply.github.com>
2025-03-02 13:29:21 -0500

    Create stops_controller.rb

---
+++ app/controllers/api/v1/stops_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class StopsController < ApplicationController
+      def index
+        stops = ExternalApiService.get_stops
+        render json: stops
+      end
+      
+      def show
+        stops = ExternalApiService.get_stops
+        stop = stops[params[:id]]
+        render json: stop
+      end
+      
+      def by_route
+        stops = ExternalApiService.get_route_stops(params[:route_id])
+        render json: stops
+      end
+    end
+  end
+endstops_controller.rb
