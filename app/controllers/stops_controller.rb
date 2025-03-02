c004b486f248998ebd3931780592e93dad3ead96
YUSRA <111371895+yrasool@users.noreply.github.com>
2025-03-02 12:55:49 -0500

    Update stops_controller.rb

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -1,2 +1,12 @@
 class StopsController < ApplicationController
+  def index
+    @stops = ExternalApiService.get_stops
+  end
+  
+  def show
+    # This will need adjustment since your FastAPI doesn't have a direct stop endpoint
+    @stop_id = params[:id]
+    @stops = ExternalApiService.get_stops
+    @stop = @stops[@stop_id]
+  end
 end
