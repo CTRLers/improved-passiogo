771e30466bae91af158cc0f2b962da8afe7e7273
YUSRA <111371895+yrasool@users.noreply.github.com>
2025-03-02 13:28:16 -0500

    Update routes_controller.rb

--- app/controllers/api/v1/routes_controller.rb
+++ app/controllers/api/v1/routes_controller.rb
@@ -13,26 +13,3 @@ module Api
     end
   end
 end
-
-# app/controllers/api/v1/stops_controller.rb
-module Api
-  module V1
-    class StopsController < ApplicationController
-      def index
-        stops = ExternalApiService.get_stops
-        render json: stops
-      end
-      
-      def show
-        stops = ExternalApiService.get_stops
-        stop = stops[params[:id]]
-        render json: stop
-      end
-      
-      def by_route
-        stops = ExternalApiService.get_route_stops(params[:route_id])
-        render json: stops
-      end
-    end
-  end
-end
