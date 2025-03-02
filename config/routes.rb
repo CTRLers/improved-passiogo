7e65f9c764d7118ca2109160796a7fc25e50dfa3
Dayne Guy <dayneguy@usf.edu>
2025-03-02 16:02:16 -0500

    Update styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 # config/routes.rb
 Rails.application.routes.draw do
   # Existing web routes
-  resources :routes, only: [:index, :show]
-  resources :stops, only: [:index, :show]
-  
+  resources :routes, only: [ :index, :show ]
+  resources :stops, only: [ :index, :show ]
+
   # API routes
   namespace :api do
     namespace :v1 do
-      resources :routes, only: [:index, :show]
-      resources :stops, only: [:index, :show] do
+      resources :routes, only: [ :index, :show ]
+      resources :stops, only: [ :index, :show ] do
         collection do
-          get 'by_route/:route_id', to: 'stops#by_route'
+          get "by_route/:route_id", to: "stops#by_route"
         end
       end
     end
