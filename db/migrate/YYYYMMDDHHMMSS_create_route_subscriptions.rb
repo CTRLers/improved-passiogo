8d5d031eab56484f5bcb9aac16dcd99335e1d568
YUSRA <111371895+yrasool@users.noreply.github.com>
2025-03-02 14:39:42 -0500

    Create YYYYMMDDHHMMSS_create_route_subscriptions.rb

---
+++ db/migrate/YYYYMMDDHHMMSS_create_route_subscriptions.rb
@@ -0,0 +1,11 @@
+class CreateRouteSubscriptions < ActiveRecord::Migration[8.0]
+  def change
+    create_table :route_subscriptions do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :route, null: false, foreign_key: true
+      t.timestamps
+    end
+    
+    add_index :route_subscriptions, [:user_id, :route_id], unique: true
+  end
+end
