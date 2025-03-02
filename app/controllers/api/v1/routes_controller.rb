e7fe3207e88fe73ecd2429cbbef9a18ac055b34e
Dayne Guy <dayneguy@usf.edu>
2025-03-02 16:23:52 -0500

    Update styling

--- app/controllers/api/v1/routes_controller.rb
+++ app/controllers/api/v1/routes_controller.rb
@@ -5,7 +5,6 @@ module Api
         routes = ExternalApiService.get_routes
         render json: routes
       end
-      
       def show
         route = ExternalApiService.get_route(params[:id])
         render json: route
