f1bc5d0e5b959e0999ed5af78beba95e4226f9ef
YUSRA <111371895+yrasool@users.noreply.github.com>
2025-03-02 14:40:23 -0500

    Create YYYYMMDDHHMMSS_create_stop_subscriptions.rb

---
+++ db/migrate/YYYYMMDDHHMMSS_create_stop_subscriptions.rb
@@ -0,0 +1,11 @@
+class CreateStopSubscriptions < ActiveRecord::Migration[8.0]
+  def change
+    create_table :stop_subscriptions do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :stop, null: false, foreign_key: true
+      t.timestamps
+    end
+    
+    add_index :stop_subscriptions, [:user_id, :stop_id], unique: true
+  end
+end
