a9517a41781f3eba9be9cdc2c61b69cb640ba0df
YUSRA <111371895+yrasool@users.noreply.github.com>
2025-03-02 13:47:09 -0500

    Update routes_controller.rb

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,1 +1,10 @@
+class RoutesController < ApplicationController
+  def index
+    @routes = ExternalApiService.get_routes
+  end
 
+  def show
+    @route = ExternalApiService.get_route(params[:id]) # Fixed params syntax
+    @stops = ExternalApiService.get_route_stops(params[:id])
+  end
+end
