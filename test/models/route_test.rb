0e03ee11d02f9088f379def777d7f08122c8b5b9
Dayne Guy <dayneguy@usf.edu>
2025-03-08 09:10:23 -0500

    Creates tests for routes and stops

--- test/models/route_test.rb
+++ test/models/route_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class RouteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    # Use latitude and longitude
+    @route = Route.new(name: "Test Route", latitude: 40.123, longitude: -74.123)
+  end
+
+  test "should be valid with valid attributes" do
+    assert @route.valid?
+  end
+
+  test "name should be present" do
+    @route.name = " "
+    assert_not @route.valid?, "Route is valid without a name"
+  end
+
+  test "name should be unique" do
+    duplicate_route = @route.dup
+    @route.save!
+    assert_not duplicate_route.valid?, "Duplicate route with same name is valid"
+  end
 end
--- test/models/stop_test.rb
+++ test/models/stop_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class StopTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    # Create a route with valid coordinates
+    route = Route.create!(name: "Route for Stop", latitude: 40.123, longitude: -74.123)
+    @stop = Stop.new(name: "Test Stop", latitude: 40.123, longitude: -74.123, route: route)
+  end
+
+  test "should be valid with valid attributes" do
+    assert @stop.valid?
+  end
+
+  test "name should be present" do
+    @stop.name = " "
+    assert_not @stop.valid?, "Stop is valid without a name"
+  end
 end
