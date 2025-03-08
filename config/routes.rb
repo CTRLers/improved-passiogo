ee2a25a807eb030a5488bdff04f61caf2bf979a3
Dayne Guy <dayneguy@usf.edu>
2025-03-08 09:13:47 -0500

    Reformatted code

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  resources :routes, only: [:index, :show]
-  resources :stops, only: [:index, :show]
+  resources :routes, only: [ :index, :show ]
+  resources :stops, only: [ :index, :show ]
 
-  resources :users, only: [:new, :create, :show, :destroy] do
-    resources :route_subscriptions, only: [:create, :destroy], param: :route_id
-    resources :stop_subscriptions, only: [:create, :destroy], param: :stop_id
+  resources :users, only: [ :new, :create, :show, :destroy ] do
+    resources :route_subscriptions, only: [ :create, :destroy ], param: :route_id
+    resources :stop_subscriptions, only: [ :create, :destroy ], param: :stop_id
   end
 
   root "home#index"
 
   namespace :api do
     namespace :v1 do
-      resources :routes, only: [:index, :show]
-      resources :stops, only: [:index, :show] do
+      resources :routes, only: [ :index, :show ]
+      resources :stops, only: [ :index, :show ] do
         collection do
           get "by_route/:route_id", to: "stops#by_route"
         end
--- test/controllers/route_subscriptions_controller_test.rb
+++ test/controllers/route_subscriptions_controller_test.rb
@@ -4,7 +4,7 @@ class RouteSubscriptionsControllerTest < ActionDispatch::IntegrationTest
   test "should create route subscription" do
     user = users(:one)
     route = routes(:one)
-    assert_difference('RouteSubscription.count', 1) do
+    assert_difference("RouteSubscription.count", 1) do
       post user_route_subscriptions_path(user, route_id: route.id)
     end
     assert_redirected_to user
@@ -15,10 +15,9 @@ class RouteSubscriptionsControllerTest < ActionDispatch::IntegrationTest
     route = routes(:one)
     # Ensure a subscription exists first, e.g.:
     user.subscribe_to_route(route)
-    assert_difference('RouteSubscription.count', -1) do
+    assert_difference("RouteSubscription.count", -1) do
       delete user_route_subscription_path(user, route_id: route.id)
     end
     assert_redirected_to user
   end
-
 end
