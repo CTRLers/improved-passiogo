b432bfa8a757666c68fe5514d7cbfb9d674ce054
Dayne Guy <dayneguy@usf.edu>
2025-03-20 20:02:23 -0400

    Properly tested stop subscription

--- test/controllers/stop_subscriptions_controller_test.rb
+++ test/controllers/stop_subscriptions_controller_test.rb
@@ -1,13 +1,19 @@
 require "test_helper"
 
 class StopSubscriptionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get stop_subscriptions_create_url
-    assert_response :success
+  test "should create stop subscription" do
+    user = users(:one)
+    stop = stops(:one)
+    post user_stop_subscriptions_path(user), params: { stop_id: stop.id }
+    assert_response :redirect   # or assert_redirected_to user_path(user) if that's expected
   end
 
-  test "should get destroy" do
-    get stop_subscriptions_destroy_url
-    assert_response :success
+  test "should destroy stop subscription" do
+    user = users(:one)
+    stop = stops(:one)
+    # Create the subscription first
+    subscription = StopSubscription.create(user: user, stop: stop)
+    delete user_stop_subscription_path(user, stop.id)
+    assert_response :redirect   # or assert_redirected_to user_path(user)
   end
 end
