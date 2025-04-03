cd5f776a9df8750e9f38ba5d2d9885251981daaa
raelynndang <166969950+raelynndang@users.noreply.github.com>
2025-04-03 11:31:14 -0400

    Update buses_test.rb

--- test/models/buses_test.rb
+++ test/models/buses_test.rb
@@ -1,39 +1,47 @@
-require 'rails_helper'
+require "test_helper"
 
-RSpec.describe Bus, type: :model do
-  let(:bus) { Bus.new(bus_number: '12345', capacity: 50, status: 'active', bus_color: 'red') }
-
-  context 'validations' do
-    it 'is valid with valid attributes' do
-      expect(bus).to be_valid
-    end
-
-    it 'is not valid without a bus number' do
-      bus.bus_number = nil
-      expect(bus).not_to be_valid
-    end
-
-    it 'is not valid without a capacity' do
-      bus.capacity = nil
-      expect(bus).not_to be_valid
-    end
-
-    it 'is not valid without a bus color' do
-      bus.bus_color = nil
-      expect(bus).not_to be_valid
-    end
-
-    it 'is not valid with a duplicate bus number' do
-      bus.save
-      duplicate_bus = bus.dup
-      expect(duplicate_bus).not_to be_valid
-    end
+class BusTest < ActiveSupport::TestCase
+  def setup
+    @bus = Bus.new(bus_number: "12345", capacity: 50, status: "active", bus_color: "red")
   end
 
-  context 'default values' do
-    it 'has a default status of active' do
-      new_bus = Bus.new(bus_number: '54321', capacity: 30, bus_color: 'blue')
-      expect(new_bus.status).to eq('active')
-    end
+  test "should be valid with valid attributes" do
+    assert @bus.valid?
+  end
+
+  test "should require a bus_number" do
+    @bus.bus_number = nil
+    assert_not @bus.valid?
+    assert_includes @bus.errors[:bus_number], "can't be blank"
+  end
+
+  test "should require a capacity" do
+    @bus.capacity = nil
+    assert_not @bus.valid?
+    assert_includes @bus.errors[:capacity], "can't be blank"
+  end
+
+  test "should require a status" do
+    @bus.status = nil
+    assert_not @bus.valid?
+    assert_includes @bus.errors[:status], "can't be blank"
+  end
+
+  test "should require a bus_color" do
+    @bus.bus_color = nil
+    assert_not @bus.valid?
+    assert_includes @bus.errors[:bus_color], "can't be blank"
+  end
+
+  test "should enforce unique bus_number" do
+    @bus.save
+    duplicate_bus = @bus.dup
+    assert_not duplicate_bus.valid?
+    assert_includes duplicate_bus.errors[:bus_number], "has already been taken"
+  end
+
+  test "should default status to active" do
+    new_bus = Bus.create(bus_number: "67890", capacity: 40, bus_color: "blue")
+    assert_equal "active", new_bus.status
   end
 end
