c377f11328a8463dbf2de679a73aa06f63ed2bec
raelynndang <166969950+raelynndang@users.noreply.github.com>
2025-04-03 11:25:13 -0400

    Update buses_test.rb

--- test/models/buses_test.rb
+++ test/models/buses_test.rb
@@ -1,50 +1,39 @@
-require 'test_helper'
+require 'rails_helper'
 
-class BusesTest < ActiveSupport::TestCase
-  # Setup method to create a valid bus instance before each test
-  def setup
-    @bus = Bus.new(bus_number: '12345', capacity: 50, status: 'active', bus_color: 'red')
+RSpec.describe Bus, type: :model do
+  let(:bus) { FactoryBot.build(:bus) }
+
+  context 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(bus).to be_valid
+    end
+
+    it 'is not valid without a bus number' do
+      bus.bus_number = nil
+      expect(bus).not_to be_valid
+    end
+
+    it 'is not valid without a capacity' do
+      bus.capacity = nil
+      expect(bus).not_to be_valid
+    end
+
+    it 'is not valid if capacity is less than 1' do
+      bus.capacity = 0
+      expect(bus).not_to be_valid
+    end
   end
 
-  # Test that the bus is valid with all fields filled
-  test "should be valid with all fields" do
-    assert @bus.valid?
+  context 'Associations' do
+    it { should belong_to(:route) }
+    it { should have_many(:bookings) }
   end
 
-  # Test that bus number cannot be empty
-  test "bus number should be present" do
-    @bus.bus_number = " "
-    assert_not @bus.valid?
-  end
-
-  # Test that bus number should be unique
-  test "bus number should be unique" do
-    duplicate_bus = @bus.dup
-    @bus.save
-    assert_not duplicate_bus.valid?
-  end
-
-  # Test that capacity should be present
-  test "capacity should be present" do
-    @bus.capacity = nil
-    assert_not @bus.valid?
-  end
-
-  # Test that capacity should be a number
-  test "capacity should be a number" do
-    @bus.capacity = "abc"
-    assert_not @bus.valid?
-  end
-
-  # Test that bus color should be present
-  test "bus color should be present" do
-    @bus.bus_color = " "
-    assert_not @bus.valid?
-  end
-
-  # Test that the status field has a default value of 'active'
-  test "status should have a default value" do
-    bus = Bus.new(bus_number: '54321', capacity: 30, bus_color: 'blue')
-    assert_equal 'active', bus.status
+  context 'Methods' do
+    it 'returns the correct bus information' do
+      bus.bus_number = '123'
+      bus.capacity = 50
+      expect(bus.info).to eq('Bus Number: 123, Capacity: 50')
+    end
   end
 end
