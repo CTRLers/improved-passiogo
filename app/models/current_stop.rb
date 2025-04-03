471d13480c7cfab81fe00c3bd7e59bca6a7b61cf
yrasool <ryusra9@gmail.com>
2025-04-02 21:45:13 -0400

    Fixed trailing whitespace issue

--- app/models/current_stop.rb
+++ app/models/current_stop.rb
@@ -1,10 +1,10 @@
 class Route < ApplicationRecord
   has_many :stops, dependent: :destroy
   has_many :messages, as: :messageable, dependent: :destroy
-  
+
   # Add the 1-to-1 relationship with current stop
-  belongs_to :current_stop, class_name: 'Stop', optional: true
-  
+  belongs_to :current_stop, class_name: "Stop", optional: true
+
   validates :name, presence: true
   validates :latitude, :longitude, presence: true, numericality: true
 end
