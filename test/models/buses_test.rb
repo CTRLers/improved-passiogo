9fa2800dd41667d893b4be29ca810be6a06b54c5
raelynndang <166969950+raelynndang@users.noreply.github.com>
2025-04-03 10:50:15 -0400

    Update buses_test.rb

--- test/models/buses_test.rb
+++ test/models/buses_test.rb
@@ -9,13 +9,25 @@ class CreateBusesTest < ActiveSupport::TestCase
     ActiveRecord::Base.connection.rollback_db_transaction
   end
 
-  test "buses table should exist" do
+  test "buses table exists" do
     assert ActiveRecord::Base.connection.table_exists?(:buses), "Table 'buses' does not exist"
   end
 
-  test "buses table should have correct columns" do
-    columns = ActiveRecord::Base.connection.columns(:buses).map(&:name)
-    assert_equal %w[id bus_number capacity status created_at updated_at], columns.sort
+  test "buses table has the correct columns" do
+    expected_columns = {
+      "id" => :integer,
+      "bus_number" => :string,
+      "capacity" => :integer,
+      "status" => :string,
+      "created_at" => :datetime,
+      "updated_at" => :datetime
+    }
+
+    buses_columns = ActiveRecord::Base.connection.columns(:buses).map { |c| [c.name, c.type] }.to_h
+
+    expected_columns.each do |col, type|
+      assert_equal type, buses_columns[col], "Column '#{col}' should be of type #{type}"
+    end
   end
 
   test "bus_number should be not null and unique" do
@@ -35,6 +47,6 @@ class CreateBusesTest < ActiveSupport::TestCase
   test "status should have a default value" do
     column = ActiveRecord::Base.connection.columns(:buses).find { |c| c.name == "status" }
     assert_not column.null, "status should not allow NULL"
-    assert_equal "active", column.default, "status default value should be 'active'"
+    assert_equal "active", column.default, "status default should be 'active'"
   end
 end
