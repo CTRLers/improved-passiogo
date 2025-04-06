a363980c2627e1aa0940d992e4102a46c0e9696b
Dayne Guy <dayneguy@usf.edu>
2025-04-06 19:17:36 -0400

    Update CI script to run server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,6 +79,4 @@ Rails.application.configure do
     authentication: "plain",
     enable_starttls_auto: true
   }
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
-  resources :users, only: [:show] do
+  resources :users, only: [ :show ] do
     resources :route_subscriptions, only: [ :create, :destroy ]
     resources :stop_subscriptions, only: [ :create, :destroy ]
   end
