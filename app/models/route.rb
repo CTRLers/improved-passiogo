34036d24fe62c8eb9b164e3a7a489b57bc3eb020
Dayne Guy <dayneguy@usf.edu>
2025-04-09 18:56:00 -0400

    Added current stop

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,9 +3,8 @@ class Route < ApplicationRecord
   has_many :messages, as: :messageable, dependent: :destroy
 
   # Add the association for the current stop.
-  belongs_to :current_stop, class_name: 'Stop', optional: true
+  belongs_to :current_stop, class_name: "Stop", optional: true
 
   validates :name, presence: true
   validates :latitude, :longitude, presence: true, numericality: true
-
 end
