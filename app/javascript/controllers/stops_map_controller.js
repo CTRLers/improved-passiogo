b392d68dcba698ef3507bf6ab4891435c66684eb
Dayne Guy <dayneguy@usf.edu>
2025-04-23 00:47:35 -0400

    fixed issue with wrong api call

--- app/javascript/controllers/stops_map_controller.js
+++ app/javascript/controllers/stops_map_controller.js
@@ -44,7 +44,7 @@ export default class extends Controller {
     //   title:    this.nameValue,
     // })
 
-    const marker = new google.maps.markerAdvancedMarkerElement({
+    const marker = new google.maps.marker.AdvancedMarkerElement({
       map:      this.map,
       position: center,
       title:    this.nameValue,
