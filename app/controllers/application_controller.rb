57f2fe8ed92ba437f9467ed92e83ffe12ded41c7
Dayne Guy <dayneguy@usf.edu>
2025-04-23 00:14:40 -0400

    fixed devise accept

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
+
 class ApplicationController < ActionController::Base
   # Shared controller logic goes here
   skip_before_action :verify_authenticity_token
   # TODO: Fix cookies
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 end
